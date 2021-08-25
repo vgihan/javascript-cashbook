fca5f86d2a3f86498a061b209971d7221aea36cf
J017_김기한 <49841765+vgihan@users.noreply.github.com>
2021-08-25 10:15:45 +0900

    layout: main page layout 구성 [J017] (#176)
    * feat: header layout 구성
    
    * fix: pug id, class 속성 #,. 키워드로 변경
    
    * feat: input bar 추가
    
    * layout: main page layout 구성
    
    * add: readme 추가

--- cashbook/routes/index.js
+++ cashbook/routes/index.js
@@ -3,7 +3,47 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/main');
+});
+router.get('/main', function(req, res, next) {
+  // 임시 데이터
+  const histories = {
+    21: [
+      {category: '교통', memo: '후불 교통비 결제', payment: '현대카드', price: '-10,000원'},
+      {category: '교통', memo: '후불 교통비 결제', payment: '현대카드', price: '-10,000원'},
+      {category: '교통', memo: '후불 교통비 결제', payment: '현대카드', price: '-10,000원'},
+    ],
+    4: [
+      {category: '교통', memo: '후불 교통비 결제', payment: '현대카드', price: '-10,000원'},
+      {category: '교통', memo: '후불 교통비 결제', payment: '현대카드', price: '-10,000원'},
+    ],
+    7: [
+      {category: '교통', memo: '후불 교통비 결제', payment: '현대카드', price: '-10,000원'},
+    ]
+  }
+  const dayInfo = {
+    21: {dayOfWeek: '월', income: 10000, expenditure: 30000},
+    4: {dayOfWeek: '화', income: 0, expenditure: 30000},
+    7: {dayOfWeek: '수', income: 0, expenditure: 30000},
+  }
+  const date = new Date();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+  res.render('main', {
+    year: year, 
+    month: month, 
+    income: '999,999원', 
+    expenditure: '999,999원',
+    numOfHistory: 9,
+    histories: histories,
+    dayInfo: dayInfo,
+  });
+});
+router.get('/calendar', function(req, res, next) {
+  res.render('caledar', { title: 'Express' });
+});
+router.get('/statistic', function(req, res, next) {
+  res.render('statistic', { title: 'Express' });
 });
 
 module.exports = router;

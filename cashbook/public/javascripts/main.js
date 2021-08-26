8dde70fa0d150d66107a3302ba2c268947078fe3
vgihan <jum654@naver.com>
2021-08-26 21:15:31 +0900

    feat: payment dropdown menu 구현

--- cashbook/public/javascripts/main.js
+++ cashbook/public/javascripts/main.js
@@ -1,22 +1,31 @@
 document.getElementById('income').addEventListener('click', incomeCheckHandler);
 document.getElementById('expenditure').addEventListener('click', expenditureCheckHandler);
-document.querySelector('.input_box.category > .dropdown_box').addEventListener('click', categoryDropdownHandler);
-document.querySelectorAll('.dropdown_category > .dropdown_item').forEach(element => {
-    element.addEventListener('click', categoryClickHandler);
+document.querySelector('.input_box.category > .dropdown_box').addEventListener('click', () => dropdownHandler('category'));
+document.querySelector('.input_box.payment > .dropdown_box').addEventListener('click', () => dropdownHandler('payment'));
+document.querySelector('.dropdown_payment > .dropdown_item:last-child').addEventListener('click', addPaymentBtnClickHandler);
+document.querySelectorAll('.dropdown_category > .dropdown_item > .content').forEach(element => {
+    element.addEventListener('click', (ev) => dropdownClickHandler(ev, 'category'));
+});
+document.querySelectorAll('.dropdown_payment > .dropdown_item > .content:not(:last-child)').forEach(element => {
+    element.addEventListener('click', (ev) => dropdownClickHandler(ev, 'payment'));
 });
 
-function categoryClickHandler(ev) {
-    const target = document.querySelector('.dropdown_box > .target');
+function addPaymentBtnClickHandler(ev) {
+    const newPayment = window.prompt('추가하실 결제수단을 입력해주세요.');
+}
+function dropdownClickHandler(ev, name) {
+    const target = document.querySelector(`.input_box.${name} > .dropdown_box > .target`);
     const selectedValue = ev.target.innerText;
     target.innerText = selectedValue;
     target.style.color = 'black';
-    document.querySelector('.input_box.category > #category').value = selectedValue;
+    console.log(selectedValue)
+    document.querySelector(`.input_box.${name} > #${name}`).value = selectedValue;
 }
-function categoryDropdownHandler() {
-    const categoryDropdown = document.querySelector('.dropdown_category');
-    if(categoryDropdown.style.display === 'none')
-        categoryDropdown.style.display = 'block';
-    else categoryDropdown.style.display = 'none';
+function dropdownHandler(name) {
+    const dropdown = document.querySelector(`.dropdown_${name}`);
+    if(dropdown.style.display === 'none')
+    dropdown.style.display = 'block';
+    else dropdown.style.display = 'none';
 }
 
 function incomeCheckHandler() {

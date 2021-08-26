b5f4a0ef3cac52b501cab9bbe1f4f93170bfc473
J017_김기한 <49841765+vgihan@users.noreply.github.com>
2021-08-26 10:15:23 +0900

    [J017] 데이터베이스 삽입 검색 기능 구현 (#339)
    * feat: header layout 구성
    
    * fix: pug id, class 속성 #,. 키워드로 변경
    
    * feat: input bar 추가
    
    * layout: main page layout 구성
    
    * add: readme 추가
    
    * add: gitignore 추가
    
    * fix: css 수정 및 앵커 경로 추가
    
    * fix: controller 분리
    
    * feat: 데이터베이스 insert, select 구현

---
+++ cashbook/controllers/postController.js
@@ -0,0 +1,17 @@
+const _model = require('../models/model');
+const model = new _model();
+
+exports.history = async (req, res, next) => {
+    const history = {
+        date: req.body.date,
+        category: req.body.category,
+        memo: req.body.memo,
+        payment: req.body.payment,
+        price: req.body.price,
+    };
+    await model.insertHistory(history);
+    res.redirect(`/main?year=${req.body.year}&month=${req.body.month}`);
+}
+exports.payment = (req, res, next) => {
+    
+}

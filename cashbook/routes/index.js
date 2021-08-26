d222298168006f557450b376a10804825718cb80
J017_김기한 <49841765+vgihan@users.noreply.github.com>
2021-08-26 10:15:23 +0900

    [J017] 데이터베이스 삽입 검색 기능 구현 (#339)
    * feat: header layout 구성
    
    * fix: pug id, class 속성 #,. 키워드로 변경
    
    * feat: input bar 추가
    
    * layout: main page layout 구성
    
    * add: readme 추가
    
    * add: gitignore 추가
    
    * fix: css 수정 및 앵커 경로 추가
    
    * fix: controller 분리
    
    * feat: 데이터베이스 insert, select 구현

--- cashbook/routes/index.js
+++ cashbook/routes/index.js
@@ -1,49 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const getController = require('../controllers/getController');
+const postController = require('../controllers/postController');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.redirect('/main');
-});
-router.get('/main', function(req, res, next) {
-  // 임시 데이터
-  const histories = {
-    21: [
-      {category: '교통', memo: '후불 교통비 결제', payment: '현대카드', price: '-10,000원'},
-      {category: '교통', memo: '후불 교통비 결제', payment: '현대카드', price: '-10,000원'},
-      {category: '교통', memo: '후불 교통비 결제', payment: '현대카드', price: '-10,000원'},
-    ],
-    4: [
-      {category: '교통', memo: '후불 교통비 결제', payment: '현대카드', price: '-10,000원'},
-      {category: '교통', memo: '후불 교통비 결제', payment: '현대카드', price: '-10,000원'},
-    ],
-    7: [
-      {category: '교통', memo: '후불 교통비 결제', payment: '현대카드', price: '-10,000원'},
-    ]
-  }
-  const dayInfo = {
-    21: {dayOfWeek: '월', income: 10000, expenditure: 30000},
-    4: {dayOfWeek: '화', income: 0, expenditure: 30000},
-    7: {dayOfWeek: '수', income: 0, expenditure: 30000},
-  }
-  const date = new Date();
-  const month = date.getMonth();
-  const year = date.getFullYear();
-  res.render('main', {
-    year: year, 
-    month: month, 
-    income: '999,999원', 
-    expenditure: '999,999원',
-    numOfHistory: 9,
-    histories: histories,
-    dayInfo: dayInfo,
-  });
-});
-router.get('/calendar', function(req, res, next) {
-  res.render('caledar', { title: 'Express' });
-});
-router.get('/statistic', function(req, res, next) {
-  res.render('statistic', { title: 'Express' });
-});
+/* GET request */
+router.get('/', getController.index);
+router.get('/main', getController.main);
+router.get('/calendar', getController.calendar);
+router.get('/statistic', getController.statistic);
+
+/* POST request */
+router.post('/history', postController.history);
+router.post('/payment', postController.payment);
 
 module.exports = router;

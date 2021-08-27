a70badce3a4a65b307b87a0f86b5315de80492ce
vgihan <jum654@naver.com>
2021-08-28 00:48:09 +0900

    feat: +/- 토글 시 category item 변경 구현

--- cashbook/public/javascripts/main.js
+++ cashbook/public/javascripts/main.js
@@ -14,13 +14,22 @@ document.querySelectorAll('.dropdown_payment > .dropdown_item > .content:not(:la
 function signClickHandler(ev) {
     const sign = document.querySelector('.input_box.price > div > input[type=hidden]');
     const span = document.querySelector('.input_box.price > div > span');
-    console.log(sign.value)
+    const incomeCategory = document.querySelectorAll('.dropdown_item.income');
+    const expenditureCategory = document.querySelectorAll('.dropdown_item.expenditure');
+    
+    document.querySelector('.input_box.category > #category').innerText = '미분류';;
+    document.querySelector('.category > .dropdown_box > .target').innerText = '선택하세요';;
+
     if(parseInt(sign.value) === -1) {
         span.innerText = '+'
         sign.value = 1;
+        incomeCategory.forEach((element) => element.style.display = 'flex');
+        expenditureCategory.forEach((element) => element.style.display = 'none');
     } else if(parseInt(sign.value) === 1) {
         span.innerText = '-'
         sign.value = -1;
+        incomeCategory.forEach((element) => element.style.display = 'none');
+        expenditureCategory.forEach((element) => element.style.display = 'flex');
     }
 }
 function addPaymentBtnClickHandler(ev) {

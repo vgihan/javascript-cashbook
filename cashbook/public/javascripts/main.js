f13247ad8341ff5e47c7ed4d957270b8f897a2cb
vgihan <jum654@naver.com>
2021-08-28 13:58:37 +0900

    fix: 내역 hover 이벤트 추가 및 초기 날짜 수정

--- cashbook/public/javascripts/main.js
+++ cashbook/public/javascripts/main.js
@@ -3,7 +3,10 @@ document.getElementById('expenditure').addEventListener('click', expenditureChec
 document.querySelector('.input_box.category > .dropdown_box').addEventListener('click', () => dropdownHandler('category'));
 document.querySelector('.input_box.payment > .dropdown_box').addEventListener('click', () => dropdownHandler('payment'));
 document.querySelector('.dropdown_payment > .dropdown_item:last-child').addEventListener('click', addPaymentBtnClickHandler);
-document.querySelector('.input_box.price > div > span').addEventListener('click', (ev) => signClickHandler(ev));
+document.querySelector('.input_box.price > div > span').addEventListener('click', (ev) => changeCategory(ev));
+document.querySelectorAll('.detail_box').forEach(element => {
+    element.addEventListener('click', (ev) => showHistoryInputbar(ev))
+});
 document.querySelectorAll('.dropdown_category > .dropdown_item > .content').forEach(element => {
     element.addEventListener('click', (ev) => dropdownClickHandler(ev, 'category'));
 });
@@ -11,13 +14,22 @@ document.querySelectorAll('.dropdown_payment > .dropdown_item > .content:not(:la
     element.addEventListener('click', (ev) => dropdownClickHandler(ev, 'payment'));
 });
 
-function signClickHandler(ev) {
+function showHistoryInputbar(ev) {
+    const selectedCategory = document.querySelector('.dropdown_box > .target');
+    const selectedPayment = document.querySelector('.dropdown_box > .target');
+    const categoryInput = document.querySelector('.input_box.category > #category');
+    const paymentInput = document.querySelector('.input_box.payment > #payment');
+    const dateInput = document.querySelector('.input_box.date > #date');
+    const memoInput = document.querySelector('.input_box.memo > #memo');
+    const priceInput = document.querySelector('.input_box.price > #price');
+}
+function changeCategory(ev) {
     const sign = document.querySelector('.input_box.price > div > input[type=hidden]');
     const span = document.querySelector('.input_box.price > div > span');
     const incomeCategory = document.querySelectorAll('.dropdown_item.income');
     const expenditureCategory = document.querySelectorAll('.dropdown_item.expenditure');
     
-    document.querySelector('.input_box.category > #category').innerText = '미분류';;
+    document.querySelector('.input_box.category > #category').innerText = '미분류';
     document.querySelector('.category > .dropdown_box > .target').innerText = '선택하세요';;
 
     if(parseInt(sign.value) === -1) {

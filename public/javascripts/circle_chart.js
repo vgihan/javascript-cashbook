f48011d6e37036da6d4e6e9ee9ad8fd29b34e77e
vgihan <jum654@naver.com>
2021-09-01 22:36:12 +0900

    feat: 통계 페이지 꺾은선 그래프 layout 구성
    - canvas를 이용하여 지출을 10000으로 나눈 값을 y 값으로 설정하여 지출
      크기간 간격을 만듦. (최대 440만원)
    - 좌표간 선을 먼저 긋고 좌표 표시는 반지름 4인 원을 그려 표현함.

--- public/javascripts/circle_chart.js
+++ public/javascripts/circle_chart.js
@@ -4,9 +4,14 @@ document.querySelectorAll('.category_list > .item').forEach(element => {
 
 function openCategoryInfo(ev) {
     const categoryIndex = {'문화/여가': '1','교통': '2','미분류': '3','식비': '4','생활': '5','월급': '6','쇼핑/뷰티': '7','의료/건강': '8'};
+    const categoryName = {'문화/여가': 'culture_data','교통': 'traffic_data','미분류': 'undefinedCategory_data','식비': 'food_data','생활': 'life_data','쇼핑/뷰티': 'shopping_data','의료/건강': 'health_data'};
     const targetCategory = ev.currentTarget.firstChild.innerText;
 
     document.querySelector('.graph_box').classList.remove('hidden');
     document.querySelectorAll('.category_box').forEach(element => element.classList.add('hidden'));
     document.querySelector(`.category_box.category_${categoryIndex[targetCategory]}`).classList.remove('hidden');
+
+    const dataElement = document.getElementById(categoryName[targetCategory]);
+    initGraph();
+    drawData(dataElement);
 }
--- public/javascripts/graph.js
+++ public/javascripts/graph.js
@@ -1,25 +1,69 @@
-window.addEventListener('load', () => {
+function initGraph() {
+    const rowArray = Array.from({length: 12}).map((v, i) => i*40);
+    const colArray = Array.from({length: 23}).map((v, i) => i*50+10);
     const canvas = document.querySelector('.graph > canvas');
     const ctx = canvas.getContext("2d");
+    ctx.clearRect(0,0,1111,450);
     ctx.beginPath();
-    drawRowLine(ctx);
-    drawColumnLine(ctx);
+    drawRowLine(ctx, rowArray);
+    drawColumnLine(ctx, colArray);
     ctx.strokeStyle = '#EEEEEE';
     ctx.stroke();
-});
+}
 
-function drawRowLine(ctx) {
-    const rowArray = Array.from({length: 12}).map((v, i) => i*40);
-    rowArray.forEach((v, i) => {
+function drawRowLine(ctx, row) {
+    row.forEach((v, i) => {
         ctx.moveTo(0, v);
         ctx.lineTo(1100, v);
     });
 }
 
-function drawColumnLine(ctx) {
-    const colArray = Array.from({length: 23}).map((v, i) => i*50);
-    colArray.forEach((v, i) => {
+function drawColumnLine(ctx, col) {
+    col.forEach((v, i) => {
         ctx.moveTo(v, 0);
         ctx.lineTo(v, 440);
+    });
+}
+
+function drawData(element) {
+    const data = Array.from({length:7}).map((v,i) => i).reduce((pre, v) => {
+        pre.push(element.getAttribute(`data-${v}`));
+        return pre;
+    }, []);
+    const canvas = document.querySelector('.graph > canvas');
+    const ctx = canvas.getContext("2d");
+    drawLine(data, ctx);
+    drawPoint(data, ctx);
+    drawText(data, ctx);
+}
+
+function drawPoint(data, ctx) {
+    ctx.beginPath();
+    data.forEach((v, i) => {
+        ctx.moveTo(i*100+10, 440+(v/10000));
+        ctx.arc(i*100+10, 440+(v/10000), 4, 0, 2*Math.PI);
+    });
+    ctx.strokeStyle = '#045D8B';
+    ctx.stroke();
+    ctx.fillStyle = '#045D8B';
+    ctx.fill();
+}
+
+function drawLine(data, ctx) {
+    ctx.beginPath();
+    data.forEach((v, i) => {
+        ctx.moveTo(i*100+10, 440+(v/10000));
+        ctx.lineTo((i+1)*100+10, 440+(data[i+1]/10000));
+    });
+    ctx.strokeStyle = '#B1C3CD';
+    ctx.stroke();
+}
+
+function drawText(data, ctx) {
+    ctx.beginPath();
+    data.forEach((v, i) => {
+        ctx.font = 'bold 20px Noto Sans KR';
+        ctx.fillStyle= '#333333';
+        ctx.fillText(-1*v, i*100+10, 440+(v/10000)-20);
     });
 }
